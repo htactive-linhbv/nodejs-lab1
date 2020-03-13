f677b295ca21be4299c718028cf423f8d574c1af
Linn <bvlinh@htactive.com>
2020-03-13 20:28:19 +0700

    chinh sua file app

---
+++ README.md
@@ -0,0 +1,5 @@
+// install các npm
+    npm install
+//khởi chạy
+    node app 4
+// nếu không nhập gì mặc  định file là 5
--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 const http = require('http');
 const fs = require('fs');
 const fetch = require('node-fetch');
-const argv = require("yargs").argv;
+//const argv = require("yargs").argv;
 
 // add module 
 const readFile = require('./readFile');
 // const saveFile = require('./saveFile.js');
+
+//get argv 
+function ktArgv(){
+    if(typeof process.argv[2]=='null'||typeof process.argv=='undefined'){
+        console.log('ban chua nhap argv .. gia tri mac dinh la 5');
+        process.argv[2]=5;
+    }
+}
+ktArgv();
+
 
 //lay url tu file json
 const links = JSON.parse(fs.readFileSync("./links.json"));
@@ -13,7 +23,7 @@ const links = JSON.parse(fs.readFileSync("./links.json"));
 //lặp tất cả link trong json
 for (link of links) {
     // đọc và ghi file từ page 1 đến n;
-    for (let i = 1; i <= argv._; i++) {
+    for (let i = 1; i <= process.argv[2]; i++) {
 
         readFile(link, i);
     }
